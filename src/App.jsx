20f334d3713ac84af75ddc7e463e35e70b731fd7
Roberto Carlos Quintero <roberto.quintero@nowports.com>
2023-02-23 22:30:22 -0300

    Font styles changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
 import "./style.css";
 
 function App() {
-  const fontSize = 10 + 4 - (2 / 2) * 15;
-  const color = "green";
-
-  return (
-    <h1 style={{ color: "red", fontSize, background: color }}>Hello world!</h1>
-  );
+  return <h1 style={{ color: "red" }}>Hello world!</h1>;
 }
 
 export default App;
